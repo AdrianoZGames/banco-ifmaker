daf5c2d7801029ccccc712416d7ba3ef0f9c83f4
Kevin Souza <kevinwillyansouza@gmail.com>
2023-06-12 11:02:14 -0400

    Adicionado o EJS

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs")
+app.set ('views', __dirname + '/views')
+
 app.get('/', (req, res) => {
     res.send('Hola Adriano!')
+})
+app.get('/inicio', (req, res) => {
+    res.render('index')
 })
 
 app.listen(port, () => {
