744ad4502dd987509728b2e8db425aa3170c34ea
Louis <le389568@gmail.com>
2023-06-19 16:56:28 -0400

    continuacao js Louis

--- public/JS/itens.js
+++ public/JS/itens.js
@@ -1,127 +1,132 @@
-/* alert('') */
-fetch("api/itens")
-.then(resposta=>resposta.json())
-.then((data)=>{
-    // let items = document.querySelector('#items');
-    let tabela = document.querySelector("#tabela")
-    for(let item of data){
-        let tr = document.createElement('tr')
-
-        let tdid = document.createElement('td')
-        tdId.textContent = item.id
-        let tdnome = document.createElement('td')
-        tdnome.textContent = item.nome
-        let tddisponivel = document.createElement('td')
-        tddisponivel.textContent = item.disponivel
-        let tddatadeaquisicao = document.createElement('td')
-        tddatadeaquisicao.textContent = item.datadeaquisicao
-
-        tr.appendChild(tdid)
-        tr.appendChild(tdnome)
-        tr.appendChild(tddisponivel)
-        tr.appendChild(tddatadeaquisicao)
-
-
-        //Adicionando um botão de excluir por linha da tabela
-        let aExcluir = document.createElement('a')
-        aExcluir.textContent = "Excluir"
-        aExcluir.classList.add("excluir")
-
-        //Adicionando uma ação quando clicar no botão
-        aExcluir.addEventListener('click',(event)=>{
-            event.preventDefault();
-            fetch("api/itens"+'/'+item.id,{
-                method:'DELETE'
-            })
-            .finally(()=>{
-                // window.location.reload();
-            })
-        })
-
-        let tdBotaoExcluir = document.createElement('td')
-        tdBotaoExcluir.appendChild(aExcluir)
-
-
-        let tdBotaoAlterar = document.createElement('td')
-        let aAlterar = document.createElement('a')
-        aAlterar.classList.add("alterar")
-        aAlterar.textContent="Alterar"
-
-        aAlterar.addEventListener('click',(event)=>{
-            event.preventDefault()
-            // O que precisamos fazer aqui?/
-
-            document.querySelector("#id").value = item.id
-            document.querySelector("#nome").value = item.nome
-            document.querySelector("#disponivel").value = item.disponivel
-            document.querySelector("#datadeaquisicao").value = item.datadeaquisicao
-            document.querySelector('button').textContent ="Alterar"
-
-            let form = document.querySelector('#form-cadastro')
-            form.addEventListener('submit',(event)=>{
-                event.preventDefault();
-                let id = document.querySelector('#id').value
-                let nome = document.querySelector('#nome').value
-                let disponivel = document.querySelector('#disponivel').value
-                let datadeaquisicao = document.querySelector('#datadeaquisicao').value
-                let itemUpdate = {
-                    id,
-                    nome,
-                    disponivel: disponivel,
-                    datadeaquisicao:datadeaquisicao
-                }
-                fetch("api/itens"+"/"+item.id,{
-                    method:"PUT",
-                    headers:{
-                        'Content-Type':'application/json'
-                    },
-                    body: JSON.stringify(itemUpdate)
-                })
-                .finally(()=>{
-                    // window.location.reload();
-                })
-                
-            })
-        })
-
-        tdBotaoAlterar.appendChild(aAlterar)
-
-
-        tr.appendChild(tdBotaoAlterar)
-        tr.appendChild(tdBotaoExcluir)
-
-        tabela.appendChild(tr)
+// Função para listar os itens
+function listarItens() {
+    fetch("api/itens")
+      .then(resposta => resposta.json())
+      .then((data) => {
+        let tabela = document.querySelector("#tabela-itens");
+        tabela.innerHTML = ""; // Limpar a tabela antes de preenchê-la novamente
+  
+        for (let item of data) {
+          let tr = document.createElement('tr');
+  
+          let tdId = document.createElement('td');
+          tdId.textContent = item.id;
+          let tdNome = document.createElement('td');
+          tdNome.textContent = item.nome;
+          let tdDisponivel = document.createElement('td');
+          tdDisponivel.textContent = item.disponivel === 0 ? "Indisponível" : 'disponivel'; // Verificar se o valor é igual a 0
+          let tdDataDeAquisicao = document.createElement('td');
+          tdDataDeAquisicao.textContent = item.datadeaquisicao;
+  
+          tr.appendChild(tdId);
+          tr.appendChild(tdNome);
+          tr.appendChild(tdDisponivel);
+          tr.appendChild(tdDataDeAquisicao);
+  
+          tabela.appendChild(tr);
+        }
+      });
+  }
+  
+  // Função para criar um novo item
+  function criarItem(item) {
+    fetch("api/itens", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    })
+      .then(response => response.json())
+      .then(() => {
+        // Item criado com sucesso, atualizar a lista
+        listarItens();
+      });
+  }
+  
+  // Função para atualizar um item existente
+  function atualizarItem(id, item) {
+    fetch("api/itens/" + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    })
+      .then(response => response.json())
+      .then(() => {
+        // Item atualizado com sucesso, atualizar a lista
+        listarItens();
+      });
+  }
+  
+  // Função para excluir um item
+  function excluirItem(id) {
+    fetch("api/itens/" + id, {
+      method: 'DELETE'
+    })
+      .then(() => {
+        // Item excluído com sucesso, atualizar a lista
+        listarItens();
+      });
+  }
+  
+  // Listar itens inicialmente
+  listarItens();
+  
+  // Event listener para o formulário de criação/atualização de itens
+  let form = document.querySelector("#cadastro");
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+    let id = document.querySelector("#id").value;
+    let nome = document.querySelector("#nome").value;
+    let disponivel = document.querySelector("#disponivel").value;
+    let datadeaquisicao = document.querySelector("#datadeaquisicao").value;
+    let item = {
+      id,
+      nome,
+      disponivel,
+      datadeaquisicao: datadeaquisicao
+    };
+  
+    if (id) {
+      // Se o ID existir, atualizar o item
+      atualizarItem(id, item);
+    } else {
+      // Caso contrário, criar um novo item
+      criarItem(item);
     }
-})
-let form = document.querySelector("#form-cadastro");
-
-form.addEventListener('submit',(event)=>{
-event.preventDefault();
-
-if(document.querySelector('button').textContent =="Alterar"){
-    return
-}
-let id = document.querySelector("#id").value;
-let nome = document.querySelector("#nome").value;
-let disponivel = document.querySelector("#disponivel").value;
-let datadeaquisicao = document.querySelector("#datadeaquisicao").value;
-let item = {
-    id,
-    nome,
-    disponivel,
-    datadeaquisicao,
-    
-}
-//console.log(item)
-fetch("api/itens",{
-    //Método
-    method: 'POST',
-    //Tipo de dado
-    headers:{
-        'Content-Type':'application/json'
-    },
-    //Quais são os dados
-    body: JSON.stringify(item)
-})
-.then(response=>response.json())
-});
+  });
+  
+  // Event listener para os botões de excluir
+  let tabela = document.querySelector("#tabela-itens");
+  
+  tabela.addEventListener('click', (event) => {
+    if (event.target.classList.contains("excluir")) {
+      let id = event.target.dataset.id;
+  
+      // Confirmar antes de excluir o item
+      if (confirm("Deseja excluir este item?")) {
+        excluirItem(id);
+      }
+    }
+  });
+  
+  // Event listener para os botões de alterar
+  tabela.addEventListener('click', (event) => {
+    if (event.target.classList.contains("alterar")) {
+      let id = event.target.dataset.id;
+      let nome = event.target.dataset.nome;
+      let disponivel = event.target.dataset.disponivel;
+      let datadeaquisicao = event.target.dataset.datadeaquisicao;
+  
+      // Preencher o formulário com os dados do item selecionado
+      document.querySelector("#id").value = id;
+      document.querySelector("#nome").value = nome;
+      document.querySelector("#disponivel").value = disponivel;
+      document.querySelector("#datadeaquisicao").value = datadeaquisicao;
+      document.querySelector('button').textContent = "Alterar";
+    }
+  });  
